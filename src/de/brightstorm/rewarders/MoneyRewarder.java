0eb2db76edb8441ff98c3326ee0a9943dccfb552
Jalapeno777 <craig@holibyte.com>
2013-06-25 09:56:43 -0400

    Updated to sent payments via Businesscraft

--- src/de/brightstorm/rewarders/MoneyRewarder.java
+++ src/de/brightstorm/rewarders/MoneyRewarder.java
@@ -1,5 +1,7 @@
 package de.brightstorm.rewarders;
 
+import java.sql.SQLException;
+
 import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
@@ -7,6 +9,7 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 
 import de.brightstorm.PDPlayer;
 import de.brightstorm.Rewarder;
+import com.eaveecraft.businesscraft.Queries;
 
 public class MoneyRewarder extends Rewarder {
 	private Economy economy;
@@ -28,7 +31,13 @@ public class MoneyRewarder extends Rewarder {
 	public void pay(PDPlayer pp) {
 		if(pp.getGroup().getLimit() >=0.D || economy.getBalance(pp.getP().getName())>pp.getGroup().getLimit()) {
 			double amount = pp.getGroup().getAmount()+(economy.getBalance(pp.getP().getName()) / 100.0D * pp.getGroup().getInterest());
-			economy.depositPlayer(pp.getP().getName(), amount);
+			try {
+				Queries.payCompany(pp.getP().getName(), amount);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//economy.depositPlayer(pp.getP().getName(), amount);
 			log(pp, amount+" "+economy.currencyNamePlural());
 		}
 	}
